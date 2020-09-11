c5588f826e98fa51065e2145ffa2bf50d862b4a1
Taylor Booth <taylorkbooth@gmail.com>
2020-09-11 16:45:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   my_family = ["terry", "tim", "nick", "ryan"]
-  my_family << "bean"
+  my_family << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
