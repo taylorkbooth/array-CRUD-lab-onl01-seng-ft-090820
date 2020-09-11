6ffb33477be95210b9ed05098db4798b3cf5f1ec
Taylor Booth <taylorkbooth@gmail.com>
2020-09-11 17:06:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   my_family = ["terry", "tim", "nick", "ryan"]
-  my_family[2]]
+  my_family[2]
 end
 
 def retrieve_first_element_from_array(array)
