02e2641cdf2b7f3925b1adc2eef2456902d26c83
Taylor Booth <taylorkbooth@gmail.com>
2020-09-11 17:01:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
   my_family = ["wow", "terry", "tim", "nick", "ryan"]
-  array = my_family.shift ("wow")
+  array = my_family.shift
 end
 
 def retrieve_element_from_index(array, index_number)
