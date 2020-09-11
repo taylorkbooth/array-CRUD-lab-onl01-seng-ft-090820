a47a1a05066230742ba1da9f4f4ec9a77ab9ed3f
Taylor Booth <taylorkbooth@gmail.com>
2020-09-11 16:41:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  my_family=[terry, tim, nick, ryan]
+  my_family = [terry, tim, nick, ryan]
 end
 
 def add_element_to_end_of_array(array, element)
